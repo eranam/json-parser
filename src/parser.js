d0b6eebf2292bdc311a402180119bb17d7657344
eranam <Eran Amar>
2014-07-23 12:10:30 +0300

    parsing null & refactoring the handlers of value extraction

--- src/parser.js
+++ src/parser.js
@@ -3,29 +3,64 @@
  */
 function Parser() {
 }
-function safeRemoveQuatations(str){
+
+function safeRemoveQuatations(str) {
     var match = /^\s*"([^"]*)"\s*$/.exec(str);
     return match ? match[1] : str;
+}
+
+var CONVERTER_FUNCS = {
+    'number': {
+        'convertFunc': Number,
+        "diagnoser": function (val) {
+            return !isNaN(val);
+        }
+    },
+    'string': {
+        'convertFunc': safeRemoveQuatations,
+        "diagnoser": function (val) {
+            return /^\s*"([^"]*)"\s*$/.test(val);
+        }
+    },
+    'boolean': {
+        'convertFunc': function (val) {
+            return val == 'true';
+        },
+        "diagnoser": function (val) {
+            return /^(true|false)/.test(val);
+        }
+    },
+    'null': {
+        'convertFunc': function () {
+            return null;
+        },
+        "diagnoser": function (val) {
+            return /^null/.test(val)
+        }
+    }
+};
+
+function extractValueFromString(data) {
+    for (var type in CONVERTER_FUNCS) {
+        if (CONVERTER_FUNCS.hasOwnProperty(type) && CONVERTER_FUNCS[type].diagnoser(data)) {
+            return CONVERTER_FUNCS[type].convertFunc(data);
+
+        }
+    }
+    return data;
 }
 
 Parser.prototype.parse = function parse(str) {
     var retObj = {};
     if (str.length > 2) {
         str = str.slice(1, str.length - 1);
-        while(str.length) {
+        while (str.length) {
             var block = str.split(',', 2)[0];
             var tokensArr = block.split(':', 2);
             var key = safeRemoveQuatations(tokensArr[0]),
                 val = safeRemoveQuatations(tokensArr[1]);
-            if (!isNaN(val)) {
-                val = Number(val);
-            } else if (/^(true|false)/.test(val)){
-                val = (val == 'true');
-            } else if(/^null/.test(val)){
-                val = null;
-            }
-            retObj[key] = val;
-            str = str.slice(block.length+1).trim();
+            retObj[key] = extractValueFromString(val);
+            str = str.slice(block.length + 1).trim();
         }
     }
     return retObj;
